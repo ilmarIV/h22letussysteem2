755c19f0ce6ccd4041fb01db4ac8c447ab846d32
Eleriin Kurs <eleriin.kurs@voco.ee>
2025-06-05 01:08:15 +0300

    kujundus

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -26,19 +26,32 @@ function Home() {
   }
 
   return (
-    <div>
-      <p>{result.lõppenud === 'ei' ? 'hääletus käib' : 'hääletus on lõppenud'}</p>
-      <h2>Küsimus:</h2>
-      <p>{result.tulemuse_nimi}</p>
-      <p><strong>Hääletanute arv:</strong> {result.haaletanute_arv}</p>
-      <p><strong>Poolt hääli:</strong> {result.poolt_haali}</p>
-      <p><strong>Vastu hääli:</strong> {result.vastu_haali}</p>
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center p-4">
+      <div className='container max-w-md bg-white shadow-md rounded-md p-6'>
+      <p
+        className={`mb-6 text-center text-2xl font-semibold ${
+        result.lõppenud === 'ei' ? 'text-green-600' : 'text-red-600'
+          }`}
+        >
+        {result.lõppenud === 'ei' ? 'Hääletus on avatud' : 'Hääletus on lõppenud'}
+      </p>
+        <div className='max-w-xl mx-auto bg-white shadow-md rounded-lg p-6 mt-6 text-gray-800'>
+          <p className="text-center text-2xl font-bold text-blue-700 mb-4"><strong>{result.tulemuse_nimi}</strong>
+          </p>
+          <div className='space-y-2 text-lg'>
+            <p><span className='font-semibold text-gray-700'>Hääletanute arv:</span> {' '}{result.haaletanute_arv}</p>
+            <p><span className="font-semibold text-green-700">Poolt hääli:</span> {' '}{result.poolt_haali}</p>
+            <p><span className="font-semibold text-red-700">Vastu hääli:</span>{' '} {result.vastu_haali}</p>
+          </div>
+        </div>
 
-      {result.lõppenud === 'ei' && (
-        <button onClick={() => navigate('/vote')}>
-          Mine hääletama
-        </button>
-      )}
+        {result.lõppenud === 'ei' && (
+          <button onClick={() => navigate('/vote')}
+            className='mb-6 w-full bg-teal-500 border border-green-800 px-4 py-2 text-white text-xl py-3 rounded hover:bg-blue-700 transition'> 
+            Mine hääletama
+          </button>
+        )}
+      </div>
     </div>
   );
 }
--- client/src/pages/Vote.js
+++ client/src/pages/Vote.js
@@ -88,43 +88,57 @@ function Vote() {
   if (!result || result.error) return <p>No result found.</p>;
 
   return (
-    <div>
-      <h2>Küsimus:</h2>
-      <p>{result.tulemuse_nimi}</p>
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center p-4">
+      <div className='container max-w-md bg-white shadow-md rounded-md p-6'>
 
-      <input
-        type="text"
-        placeholder="Sinu nimi"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-
-      <div>
-        <label>
-          <input
-            type="radio"
-            value="poolt"
-            checked={valik === 'poolt'}
-            onChange={() => setValik('poolt')}
-          />
-          Poolt
+        
+        <p className="text-center text-2xl font-bold text-blue-700 mb-4">{result.tulemuse_nimi}</p>
+        <label className="block text-teal-800 font-semibold mb-1" htmlFor="question">
+        Sisesta oma nimi
         </label>
+        <input
+          type="text"
+          placeholder="Sinu nimi"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="w-full px-3 py-2 border border-gray-300 rounded resize-none mb-20"
 
-        <label>
-          <input
-            type="radio"
-            value="vastu"
-            checked={valik === 'vastu'}
-            onChange={() => setValik('vastu')}
-          />
-          Vastu
-        </label>
+        />
+
+        <div className='flex flex-col sm:flex-row items-start sm:items-center gap-6 mt-6 mb-8'>
+          <label className='flex items-center gap-2 text-lg text-gray-800'>
+            <input
+              type="radio"
+              value="poolt"
+              checked={valik === 'poolt'}
+              onChange={() => setValik('poolt')}
+              className='accent-green-600 w-5 h-5'
+            />
+            Poolt
+          </label>
+
+          <label className='flex items-center gap-2 text-lg text-gray-800'>
+            <input
+              type="radio"
+              value="vastu"
+              checked={valik === 'vastu'}
+              onChange={() => setValik('vastu')}
+              className='accent-red-600 w-5 h-5'
+            />
+            Vastu
+          </label>
+        </div>
+
+        <button onClick={handleVote}
+          className='mb-6 w-full bg-teal-500 border border-green-800 px-4 py-2 text-white text-xl py-3 rounded hover:bg-green-700 transition mb-20'> 
+          Hääleta</button>
+        <p>{message}</p>
+
+        <button onClick={() => navigate('/')}
+          className=' w-full bg-gray-700 border border-green-800 px-4 py-2 text-white text-xl py-3 rounded-md shadow hover:bg-blue-700 transition'>
+          Tagasi pealehele
+        </button>
       </div>
-
-      <button onClick={handleVote}>Hääleta</button>
-      <p>{message}</p>
-
-      <button onClick={() => navigate('/')}>Tagasi pealehele</button>
     </div>
   );
 }
